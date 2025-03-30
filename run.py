76e6566c0fde91eaf76cbda80d3ef0a38a2d8b69
vlongle <vietlong.lenguyen@gmail.com>
2025-03-29 23:06:12 -0400

    update the download data script

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 import argparse
 import os
 from utils import on_desktop
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--obj_id", type=str, help="Objaverse object ID to process")
+    parser.add_argument("--obj_path", type=str, help="Path to the object file to process", default=None)
     parser.add_argument("--num_images", type=int, help="Number of images to render", default=200)
     parser.add_argument("--train_steps", type=int, help="Number of training steps", default=5_000)
     parser.add_argument("--camera_dist_min", type=float, help="Minimum camera distance", default=1.2)
@@ -16,33 +18,31 @@ if __name__ == "__main__":
     is_on_desktop = on_desktop()
     path_prefix = "/mnt/kostas-graid/datasets/vlongle/diffphys3d" if not is_on_desktop else "."
 
-    download_cmd = f"python download_objaverse.py --obj_id {args.obj_id}"
-    blender_render_cmd = f'blender --background --python generate_blendernerf_data.py -- --obj_id {args.obj_id} --num_images {args.num_images} --format NGP --camera_dist_min {args.camera_dist_min} --camera_dist_max {args.camera_dist_max} --output_dir {path_prefix}/data'
+    # Only download if obj_path is not provided
+    if args.obj_path is None:
+        download_cmd = f"python download_objaverse.py --obj_id {args.obj_id}"
+        # os.system(download_cmd)
+    
+    # Use obj_path if provided, otherwise use obj_id
+    obj_param = f"--obj_path {args.obj_path}" if args.obj_path is not None else f"--obj_id {args.obj_id}"
+    
+    blender_render_cmd = f'blender --background --python generate_blendernerf_data.py -- {obj_param} --num_images {args.num_images} --format NGP --camera_dist_min {args.camera_dist_min} --camera_dist_max {args.camera_dist_max} --output_dir {path_prefix}/data'
     if args.transparent_bg:
         blender_render_cmd += ' --transparent_bg'
     if not is_on_desktop:
         blender_render_cmd = f'export PATH="/mnt/kostas-graid/sw/envs/vlongle/blender/blender-4.3.2-linux-x64:$PATH"; {blender_render_cmd}'
     blender_render_cmd += f" --scene_scale {args.scene_scale}"
 
-    
+    # Use obj_id for the rest of the pipeline regardless of input method
     convert_cmd = f"python convert.py --obj_id {args.obj_id} --data_dir {path_prefix}/data"
     
-
-
     method = "f3rm"
     # method = "nerfacto"
     train_cmd = f"ns-train {method} --data {path_prefix}/data/{args.obj_id} --max-num-iterations {args.train_steps} --viewer.quit-on-train-completion True --save_only_latest_checkpoint False --output_dir {path_prefix}/outputs"
 
-
     print("train_cmd", train_cmd)
 
-
-
-
-    # os.system(download_cmd)
     # os.system(blender_render_cmd)
-
-
     os.system(convert_cmd)
     os.system(train_cmd)
     
@@ -56,12 +56,9 @@ if __name__ == "__main__":
     ns_render_cmd = f"ns-render dataset --load-config {config_path} --output-path {render_output_dir} --split=train --rendered_output_names=rgb"
     os.system(ns_render_cmd)
 
-
     voxel_cmd = f"python voxel_to_pc.py --scene {config_path} --output {render_output_dir}/clip_features.npz"
-
     os.system(voxel_cmd)
 
     voxel_pc_cmd = voxel_cmd + " --extract_pc"
-
     os.system(voxel_pc_cmd)
 
